f2b61537f8882e78abe976e78b80d3f392dedfe9
jill <zjiedo@163.com>
2020-09-23 17:41:56 +0800

    'init'

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,42 @@
+<template>
+<h1>{{ count }}</h1>
+<h1>{{ double }}</h1>
+{{number}}
+<button @click="increase">+1</button>
+<button @click="play">+1</button>
+</template>
+
+<script lang="ts">
+import {
+  defineComponent,
+  ref,
+  computed
+} from "vue";
+
+export default defineComponent({
+  name: "Hello",
+  setup() {
+    // ref 是一个函数，它接受一个参数，返回的就是一个神奇的 响应式对象 。我们初始化的这个 0 作为参数包裹到这个对象中去，在未来可以检测到改变并作出对应的相应。
+    const count = ref(0);
+    const double = computed(() => {
+      return count.value * 2;
+    });
+    const increase = () => {
+      count.value++;
+    };
+    const number = ref(1)
+    const play = () => {
+      number.value++;
+      console.log(number.value)
+    }
+
+    return {
+      count,
+      increase,
+      play,
+      double,
+      number
+    };
+  }
+});
+</script>
---
+++ src/views/About.vue
@@ -0,0 +1,38 @@
+<template>
+<div class="about">
+  <h1>{{ count }}</h1>
+  <h1>{{ double }}</h1>
+  <button @click="increase">+1</button>
+</div>
+</template>
+
+<script lang="ts">
+import {
+  computed,
+  defineComponent,
+  ref,
+  toRefs,
+  reactive
+} from 'vue'
+interface DataProps {
+  double: number,
+    count: number,
+    increase: () => void
+}
+export default defineComponent({
+  name: 'jill',
+  setup() {
+    const data: DataProps = reactive({
+      double: computed(() => data.count * 2),
+      count: 2,
+      increase: () => {
+        data.count++
+      }
+    })
+    const refData = toRefs(data)
+    return {
+      ...refData
+    }
+  }
+})
+</script>
