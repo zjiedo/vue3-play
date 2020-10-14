10a5937757c26b1dede1e35c0d8a3b7031c60fc3
jill <zjiedo@163.com>
2020-10-14 11:07:09 +0800

    feature: ts

--- src/hooks/useURLLoader.ts
+++ src/hooks/useURLLoader.ts
@@ -1,8 +1,8 @@
 import { ref } from "vue";
 import axios from "axios";
 
-function useURLLoader(url: string) {
-  const result = ref(null);
+function useURLLoader<T>(url: string) {
+  const result = ref<T | null>(null);
   const loading = ref(true);
   const loaded = ref(false);
   const error = ref(null);
@@ -28,3 +28,34 @@ function useURLLoader(url: string) {
 }
 
 export default useURLLoader;
+
+// import { ref } from "vue";
+// import axios from "axios";
+
+// function useURLLoader(url: string) {
+//   const result = ref(null);
+//   const loading = ref(true);
+//   const loaded = ref(false);
+//   const error = ref(null);
+
+//   axios
+//     .get(url)
+//     .then(rawData => {
+//       loading.value = false;
+//       loaded.value = true;
+//       result.value = rawData.data;
+//     })
+//     .catch(e => {
+//       error.value = e;
+//       loading.value = false;
+//     });
+
+//   return {
+//     loading,
+//     loaded,
+//     error,
+//     result
+//   };
+// }
+
+// export default useURLLoader;
